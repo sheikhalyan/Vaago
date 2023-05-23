33b1769784797233eb429950ccae5722a9123b75
sheikhalyan <alyantaj789@gmail.com>
2023-05-23 18:24:39 +0500

    Reservation Added on Admin Panel

---
+++ Vaago/Controllers/Admin/AdminReservationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vaago.Models;
+
+namespace Vaago.Controllers.Admin
+{
+    public class AdminReservationController : Controller
+    {
+        VaagoProjectEntities DB = new VaagoProjectEntities();
+        // GET: AdminReservation
+        public ActionResult Index()
+        { 
+            List<Reservation> reservationList = DB.Reservations.ToList();
+
+            return View("~/Views/AdminReservation/AdminReservation.cshtml", reservationList);
+            
+            //return View();
+        }
+    }
+}

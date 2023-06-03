7481646611ab75b0a3064487243398fab1da44ad
sheikhalyan <alyantaj789@gmail.com>
2023-06-03 23:43:46 +0500

    Updated!!!

--- Vaago/Models/Model1.Designer.cs
+++ Vaago/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Vaago-Alyan2\Vaago\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\alyan\Desktop\OOAD\OOADProject\Project\Vaago\Vaago\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
